c67cfbcd49684f8f011db9bbd521f20cb408c568
spicata <p.ixelandian@gmail.com>
2024-01-16 19:13:34 +0800

    this day

--- shake_text.py
+++ shake_text.py
@@ -4,9 +4,9 @@ in_text = input("> ")
 time = float(input("> "))
 in_text = in_text.replace(" ", "⠀")
 out_text = ""
-front_wrapper = "<div><p>"
+front_wrapper = "<div class=\"inner\"><p>"
 back_wrapper = "</p></div>"
 for i in range(len(in_text)):
     time_delay = str(random.uniform(0, time))[:6]
     out_text = out_text + "<span style=\"animation-delay: " + time_delay + "s\">" + list(in_text)[i] + "</span>"
-print(out_text)
+print(front_wrapper + out_text + back_wrapper)
